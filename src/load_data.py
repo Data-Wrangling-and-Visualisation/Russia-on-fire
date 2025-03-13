02a31c90b402e7f65e27e198471a96cee157403e
resent02 <remal11151@gmail.com>
2025-03-13 18:34:02 +0300

    loading data from .env small fix for loading data

--- src/load_data.py
+++ src/load_data.py
@@ -1,11 +1,23 @@
+import os
+
 import pandas as pd
 import psycopg2
+from dotenv import load_dotenv
 from psycopg2.extras import execute_values
 
-# Database connection
-conn = psycopg2.connect(
-    dbname="fires_db", user="postgres", password="passw", host="localhost"
-)
+load_dotenv()
+
+
+db_config = {
+    "dbname": os.getenv("DB_NAME"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT"),
+}
+
+# Connect to PostgreSQL
+conn = psycopg2.connect(**db_config)
 cursor = conn.cursor()
 
 # Load CSV into a Pandas DataFrame
